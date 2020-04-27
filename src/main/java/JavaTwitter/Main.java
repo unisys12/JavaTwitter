0aaf331767e0c5c6ded146bd8496ac417e91264f
unisys12 <unisys12@gmail.com>
2020-04-26 22:48:49 -0500

    inital commit

---
+++ src/main/java/JavaTwitter/Main.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaTwitter;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ *
+ * @author unisy
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LocalTime currentTime = LocalTime.now();
+
+        System.out.print("The time is : " + currentTime);
+        
+        if(currentTime.isAfter(LocalTime.parse("22:00:00.000000000"))) {
+            // Application Logic goes here!
+            System.out.print("DO SOMETHING! It's the correct time!");
+        } else {
+            // Don't do anything
+            System.out.print("Not the correct time to do something");
+        }
+    }
+    
+}
